ec7c587ee6a94c4b463dcf521be895a3dc7f8b3e
kn58buff <kn58@buffalo.edu>
2026-03-13 08:54:37 -0400

    Draft of db schema

---
+++ sql/raw/schema.sql
@@ -0,0 +1,72 @@
+/*
+Columns of the dataset.
+
+[
+    'Type', 'Days for shipping (real)', 'Days for shipment (scheduled)',
+    'Benefit per order', 'Sales per customer', 'Delivery Status',
+    'Late_delivery_risk', 'Category Id', 'Category Name', 'Customer City',
+    'Customer Country', 'Customer Email', 'Customer Fname', 'Customer Id',
+    'Customer Lname', 'Customer Password', 'Customer Segment',
+    'Customer State', 'Customer Street', 'Customer Zipcode',
+    'Department Id', 'Department Name', 'Latitude', 'Longitude', 'Market',
+    'Order City', 'Order Country', 'Order Customer Id',
+    'order date (DateOrders)', 'Order Id', 'Order Item Cardprod Id',
+    'Order Item Discount', 'Order Item Discount Rate', 'Order Item Id',
+    'Order Item Product Price', 'Order Item Profit Ratio',
+    'Order Item Quantity', 'Sales', 'Order Item Total',
+    'Order Profit Per Order', 'Order Region', 'Order State', 'Order Status',
+    'Order Zipcode', 'Product Card Id', 'Product Category Id',
+    'Product Description', 'Product Image', 'Product Name', 'Product Price',
+    'Product Status', 'shipping date (DateOrders)', 'Shipping Mode'
+]
+
+Based on the columns.
+
+Tables:
+
+Customers:
+    Customer ID, Fname, Lname, Email, Password, SegmentID, Sales per customer (?)
+
+CustomerAddress
+    Address ID, Customer ID, Customer Country, Customer State, Customer City, Customer Street, Customer Zipcode
+
+CustomerSegment
+    Segment ID, Customer Segment
+
+Departments
+    Department ID, Department Name, Latitude, Longitude
+
+Categories
+    Category ID, Category Name
+
+Products
+    Product Card ID, Product Category ID, Product Description, Product Image, Product Name, Product Price, Product Status
+
+Markets
+    MarketID, Market Name, Region iD
+
+Orders
+    Order ID, Order Customer ID, Order Item ID, Order Product ID, Order Date, Order Status,
+    Order Region, Order Country, Order State, Order City, Order Zipcode,
+    Profit Per Order
+
+OrderItems
+    Order Item ID, OrderID, Order Item Product Price, Order Item Profit Ratio, Order Item Quantity,
+    Order Item Total, Order Item Discount, Order Item Discount Rate
+
+
+ShippingModes
+    Shipping Mode ID, Shipping Mode
+
+Shipments
+    ShipmentID, Order ID, Shipping Mode ID, Shipping Date, Scheduled Delivery, Actual Delivery, Delivery Status,
+
+Regions
+    RegionID, Region Name
+
+Countries
+    CountryID, CountryName, RegionID
+PaymentTypes
+    PaymentTypeID, PaymentTypeName
+
+*/
