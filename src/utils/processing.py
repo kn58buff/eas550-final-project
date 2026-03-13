9fa2a43eb2b1952db8bb020f2a74610ff2f8be0a
kn58buff <kn58@buffalo.edu>
2026-03-13 00:54:30 -0400

    Process data

--- src/utils/processing.py
+++ src/utils/processing.py
@@ -1,34 +1,60 @@
 import os
 import kagglehub
 from pathlib import Path
+import pandas as pd
 
 
 BASE_DIR = Path(__file__).resolve().parent
 DATA_PATH = BASE_DIR / "../../data/DataCoSupplyChainDataset.csv"
 
+class DataProcessor:
+    def __init__(self, data_path = DATA_PATH):
+        self.data_path = data_path
+    
+    def load_data(self):
+        """
+            Loads the dataset into a pandas dataframe.
 
-def check_and_download_data(file_path):
-    """
-        Checks if the dataset is already downloaded.
-        file_path should be ../../data/DataCoSupplyChainDataset.csv
+            If the file does not exist, then it will be downloaded to the above file path.
+        """
 
-        If the file does not exist, then it will be downloaded to the above file path.
-    """
-    if file_path.exists():
-        print(f"File '{file_path}' already exists. Skipping download.")
-    else:
-        kagglehub.dataset_download(
-            "shashwatwork/dataco-smart-supply-chain-for-big-data-analysis",
-            output_dir=str(file_path.parent),
-        )
+        # Download dataset if it does not exist
+        if self.data_path.exists():
+            print(f"File '{self.data_path}' already exists. Skipping download.")
+        else:
+            kagglehub.dataset_download(
+                "shashwatwork/dataco-smart-supply-chain-for-big-data-analysis",
+                output_dir=str(self.data_path.parent),
+            )
+        
+        df = pd.read_csv(self.data_path, encoding = "ISO-8859-1")
 
-def preprocess(df):
-    """
-        Preprocess the dataframe.
+        return df
 
-        TODO:
-            Check null / missing values, decide how to handle them
-            Check data types and convert to correct type if needed.
-    """
+    def check_data(df):
+        """
+            Preprocess the dataframe.
 
-check_and_download_data(DATA_PATH)
+            Checks for null and missing values.
+
+            TODO:
+                Check data types and convert to correct type if needed.
+        """
+        # Check 1: Missing values
+        print(f"Dataset size: {df.shape}")
+
+        nulls = df.isnull().sum().where(lambda x: x > 0).dropna()
+        
+        for index, value in nulls.items():
+            print(f"{index}: {int(value)} missing values; {100 * int(value) / len(df):.4f}% of data")
+
+        # Drop Product Description, every entry is missing this
+        df.drop(columns=["Product Description"], inplace=True)
+
+        print("Dropped column Product Description")
+
+        # Drop Order Zipcode - not necessarily important info
+        df.drop(columns=["Order Zipcode"], inplace=True)
+        print("Dropped column Order Zipcode")
+
+        return df
