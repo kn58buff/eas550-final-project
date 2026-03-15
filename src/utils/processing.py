ca0755499a12cfae9ef10274fcba92be5f32aafc
kn58buff <kn58@buffalo.edu>
2026-03-15 00:45:51 -0400

    Complete processing

--- src/utils/processing.py
+++ src/utils/processing.py
@@ -4,6 +4,7 @@ from pathlib import Path
 import pandas as pd
 
 
+
 BASE_DIR = Path(__file__).resolve().parent
 DATA_PATH = BASE_DIR / "../../data/DataCoSupplyChainDataset.csv"
 
@@ -11,7 +12,7 @@ class DataProcessor:
     def __init__(self, data_path = DATA_PATH):
         self.data_path = data_path
     
-    def load_data(self):
+    def _load_data(self):
         """
             Loads the dataset into a pandas dataframe.
 
@@ -32,54 +33,138 @@ class DataProcessor:
         return df
     
     # Reminder to have these cleaning functions as part of the cleaning suite/pipeline.
-    def rename_columns(self, df):
+    def _rename_columns(self, df):
         """
         Convert column names to clean snake_case format.
         """
 
         df.columns = (df.columns.str.strip().str.lower().str.replace(" ", "_").str.replace(r"[()]", "", regex=True).str.replace(r"[^a-z0-9_]", "", regex=True))
 
+        df = df.rename(columns = {
+            "type": "payment_type",
+            "days_for_shipping_real": "days_shipping_real",
+            "days_for_shipment_scheduled": "days_shipping_scheduled",
+            "customer_city": "city",
+            "customer_country": "country",
+            "customer_email": "email",
+            "customer_fname": "first_name",
+            "customer_lname": "last_name",
+            "customer_segment": "segment",
+            "customer_state": "state",
+            "customer_street": "street",
+            "customer_zipcode": "zipcode",
+            "order_date_dateorders": "order_date",
+            "order_item_discount": "discount",
+            "order_item_discount_rate": "discount_rate",
+            "order_item_profit_ratio": "profit_ratio",
+            "order_item_quantity": "quantity",
+            "order_item_total": "item_total",
+            "product_card_id": "product_id",
+            "shipping_date_dateorders": "shipping_date",
+            })
+
         print("Columns renamed successfully.")
         print(df.columns)
 
         return df
 
-    def check_data(self, df):
+    def _check_data(self, df):
         """
             Preprocess the dataframe.
+            Checks for null and missing values. Checks for duplicate values.
 
-            Checks for null and missing values.
-
-            TODO:
-                Check data types and convert to correct type if needed.
-                Remove columns: [Customer Email, Customer Password] -> both have 1 value (XXXXXXXXX), probably scrubbed from data 
-
-                Decide to remove [Product Status] or not. Every item in this dataset is available (product status = 0), but could justify keeping it as a design choice for the possibility of unavailable items.
+            Removed columns: product_description and customer_password
         """
-        # Check 1: Missing values
         print(f"Dataset size: {df.shape}")
 
+        # Check #1: Duplicates
+        count_of_duplicates = df.duplicated().sum()
+        print(f"Number of duplicate rows: {count_of_duplicates}")
+
+        # Check 2: Missing values
         nulls = df.isnull().sum().where(lambda x: x > 0).dropna()
         
         for index, value in nulls.items():
             print(f"{index}: {int(value)} missing values; {100 * int(value) / len(df):.4f}% of data")
 
         # Drop Product Description, every entry is missing this
-        df.drop(columns=["Product Description"], inplace=True)
+        df.drop(columns=["product_description"], inplace=True)
 
         print("Dropped column Product Description")
 
-        # Drop Order Zipcode - not necessarily important info
-        df.drop(columns=["Order Zipcode"], inplace=True)
-        print("Dropped column Order Zipcode")
+        # Drop customer password, it has been scrubbed
+        df.drop(columns = ["customer_password"], inplace = True)
+        print("Dropped column Customer Password")
 
         return df
+    
+    def _split_data(self, df):
+        """
+            Splits the data into multiple DataFrames corresponding to each table in the database.
+        """
+        SCHEMA = {
+            "departments": {
+                "columns": ["department_id", "department_name"],
+                "primary": ["department_id"]
+            },
+            "categories": {
+                "columns": ["category_id", "category_name", "department_id"],
+                "primary": ["category_id"]
+            },
+            "products": {
+                "columns": ["product_id", "product_name", "product_price", "product_status", "product_image", "category_id"],
+                "primary": ["product_id"]
+            },
+            "customers": {
+                "columns": ["customer_id", "first_name", "last_name",
+                            "email", "segment", "street",
+                            "city", "state", "country",
+                            "zipcode", "latitude", "longitude", "market"],
+                "primary": ["customer_id"]
+            },
+            "orders": {
+                "columns": ["order_id", "order_customer_id", "order_date", "shipping_date",
+                            "order_status", "shipping_mode", "delivery_status", "late_delivery_risk",
+                            "order_city", "order_state", "order_country", "order_zipcode",
+                            "order_region", "days_shipping_real", "days_shipping_scheduled", "order_profit_per_order",
+                            "payment_type"],
+                "primary": ["order_id"]
+            },
+            "order_items": {
+                "columns": ["order_item_id", "order_id", "product_id", "quantity",
+                            "product_price", "discount", "discount_rate", "profit_ratio",
+                            "item_total", "sales", "benefit_per_order", "sales_per_customer"],
+                "primary": ["order_item_id"]
+            }
+        }
 
-obj1 = DataProcessor(DATA_PATH)
-raw_df = obj1.load_data()
-print(raw_df)
+        tables = {}
 
-df = obj1.rename_columns(raw_df)
-print(df.columns)
-print(df.shape)
-# print(df["delivery_status"].unique())
+        for table_name, config in SCHEMA.items():
+            cols = config["columns"]
+            keys = config["primary"]
+
+            table_df = df[cols].drop_duplicates(subset = keys)
+
+            tables[table_name] = table_df
+
+        # After splitting, need to rename columns that are present in multiple tables (non keys)
+        tables["order_items"].rename(columns = {"product_price": "order_item_product_price"}, inplace = True)
+
+        print(f"Split data into tables corresponding to schema.")
+        return tables
+
+    def run(self):
+        df = self._load_data()
+        df = self._rename_columns(df)
+        df = self._check_data(df)
+        tables = self._split_data(df)
+
+        print(f"Final shape of data: {df.shape}")
+        print(f"Tables created: {len(tables)}")
+        
+        return tables
+
+processor = DataProcessor()
+
+tables = processor.run()
